f456bda01a43f8571f950cfa9c98ad04bda6781e
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-30 15:13:25 +0530

    Styled restaurant lists

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -84,30 +84,37 @@ function List(props) {
           });
       };
   return (<>
-  <div className='flex justify-between mt-4 bg-gray-100 px-4'>
-    <div className='mt-4'>
-    <input className='border-2 rounded-lg border-black mr-4 pl-2' type="text" placeholder="Search by name" value={searchName} onChange={onChangeSearchName} />
-    <button className="border-lime-400 border-2 px-2 py-1 rounded-xl text-sm" type="button" onClick={findByName}>Search</button>
+  <div className='flex justify-around mt-4 bg-gray-100 px-4'>
+  <div class="flex items-center mt-4 mr-8">   
+        <label for="simple-search" class="sr-only">Search</label>
+        <div class="relative w-full">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by Name" value={searchName} onChange={onChangeSearchName} required/>
+        </div>
+        <button type="submit" onClick={findByName} class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
     </div>
-    <div className='mt-4'>
-    <input className='border-2 rounded-lg border-black mr-4 pl-2' type="text" placeholder="Search by zip" value={searchZip} onChange={onChangeSearchZip} />
-    <button className="border-lime-400 border-2 px-2 py-1 rounded-xl text-sm" type="button" onClick={findByZip}>Search</button>
+    <div class="flex items-center mt-4 mr-8">   
+        <label for="simple-search" class="sr-only">Search</label>
+        <div class="relative w-full">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by Zip" value={searchZip} onChange={onChangeSearchZip} required/>
+        </div>
+        <button type="submit" onClick={findByZip} class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
     </div>
-    <div className='mt-4'>
-            <select onChange={onChangeSearchCuisine} className='mr-4'>
+    <div className='mt-4 mr-8'>
+            <select onChange={onChangeSearchCuisine} className='mr-4 p-2'>
                  {cuisines.map(cuisine => {
+                  console.log(cuisines)
                     return (
-                    <option value={cuisine}> {cuisine.substr(0, 20)} </option>
+                    <option value={cuisine}> {cuisine} </option>
                     )
                 })}
             </select>
-            <button
-              type="button"
-              className='border-lime-400 border-2 px-2 py-1 rounded-xl text-sm'
-              onClick={findByCuisine}
-            >
-              Search
-            </button>
+            <button type="submit" onClick={findByCuisine} class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
             </div>
   </div>
     <div className='grid grid-cols-3 bg-gray-100'>
