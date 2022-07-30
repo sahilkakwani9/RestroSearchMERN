13d934a17a1a870c6e49c38443ae15662867f99b
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-30 17:17:48 +0530

    fea: responsive list

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -84,8 +84,8 @@ function List(props) {
           });
       };
   return (<>
-  <div className='flex justify-around mt-4 bg-gray-100 px-4'>
-  <div class="flex items-center mt-4 mr-8">   
+  <div className='flex justify-around pt-4 bg-gray-100 h-full px-4'>
+  <div class="flex items-center pt-4 md:ml-8">   
         <label for="simple-search" class="sr-only">Search</label>
         <div class="relative w-full">
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -93,9 +93,9 @@ function List(props) {
             </div>
             <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by Name" value={searchName} onChange={onChangeSearchName} required/>
         </div>
-        <button type="submit" onClick={findByName} class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+        <button type="submit" onClick={findByName} class="p-2.5 ml-6 md:ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
     </div>
-    <div class="flex items-center mt-4 mr-8">   
+    <div class="hidden md:flex items-center mt-4 mr-8">   
         <label for="simple-search" class="sr-only">Search</label>
         <div class="relative w-full">
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -105,7 +105,7 @@ function List(props) {
         </div>
         <button type="submit" onClick={findByZip} class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
     </div>
-    <div className='mt-4 mr-8'>
+    <div className='hidden md:block mt-4 mr-8'>
             <select onChange={onChangeSearchCuisine} className='mr-4 p-2'>
                  {cuisines.map(cuisine => {
                   console.log(cuisines)
@@ -117,7 +117,7 @@ function List(props) {
             <button type="submit" onClick={findByCuisine} class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
             </div>
   </div>
-    <div className='grid grid-cols-3 bg-gray-100'>
+    <div className='block md:grid grid-cols-3 pt-4 bg-gray-100'>
         {
             
             restaurants.map((r)=>{
